e0902db1da7e4252b1cccc61822676c9162d7fa0
Kayleigh Watts <kwatts949@gmail.com>
2022-10-25 18:02:20 +0100

    Add comments to app"

--- app.rb
+++ app.rb
@@ -5,16 +5,16 @@ DatabaseConnection.connect('music_library') # connect to main database here
 
 album_repository = AlbumRepository.new
 
-album_repository.all.each do |album|
+album_repository.all.each do |album| # all method in action
   p album
 end
 
-album = album_repository.find(2)
+album = album_repository.find(3) # find method in action
   puts album.title
 
-album_1 = Album.new
+album_1 = Album.new # create method in action
 album_1.title = 'Mania'
 album_1.release_year = "2018"
 
-# album = album_repository.create(album_1)
-  # puts album_1.title
+album = album_repository.create(album_1)
+  puts album_1.title
